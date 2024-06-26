8b26bcdbb060c8730abccbe9b51c26d24ebf9e86
Yunwei Dai <daiyunwei1998@gmail.com>
2024-06-27 06:44:36 +0800

    Add JSDoc

--- Week-2/Assignment-1/functionAndArray.js
+++ Week-2/Assignment-1/functionAndArray.js
@@ -1,44 +1,44 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.max = max;
+exports.findPosition = findPosition;
 /**
  * Find the max number in an array
  * @param {Array<number>} an input array of numbers
- * @returns {number} max number in the array 
+ * @returns {number} max number in the array
  */
-export function max(numbers:Array<number>) {
+function max(numbers) {
     // your code here, for-loop method preferred
     // check code for empty input
     if (numbers.length == 0) {
         throw new Error('Invalid input: Input must be a non empty array');
     }
-
-    let currentMax = -Infinity; // set a initial value of negative infinity
-    for (let n of numbers) {  // loop through all element
-        if (n > currentMax) {   // if a larger value is found
-            currentMax = n;    // record that larger value
+    var currentMax = -Infinity; // set a initial value of negative infinity
+    for (var _i = 0, numbers_1 = numbers; _i < numbers_1.length; _i++) { // loop through all element
+        var n = numbers_1[_i];
+        if (n > currentMax) { // if a larger value is found
+            currentMax = n; // record that larger value
         }
     }
-    return currentMax;   // return the larger value
+    return currentMax; // return the larger value
 }
-
 /**
  * Description
  * @param {Array<number>} input array of numbers
  * @param {number} target number
  * @returns {number} index of target number's first occurence, return -1 if not found
  */
-export function findPosition(numbers:Array<number>, target:number) {
+function findPosition(numbers, target) {
     // your code here, for-loop method preferred
-    
     // check code for empty input
     if (numbers.length == 0) {
         throw new Error('Invalid input: Input must be a non empty array');
     }
-
     // return target index when matched
-    for (let i = 0 ; i < numbers.length; i++) {
+    for (var i = 0; i < numbers.length; i++) {
         if (numbers[i] === target) {
             return i;
         }
     }
     return -1;
 }
-
