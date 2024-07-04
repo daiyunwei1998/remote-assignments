df7d155e19c844cbb4478a8edbd1ebd6f1d6d2b4
Yunwei Dai <daiyunwei1998@gmail.com>
2024-07-04 15:31:45 +0800

    refactor: Support Names with a larger charset by using URLencoding and decoding before sent to cookie

--- Week-3/Assignments/src/main/java/com/yun_weidai/demo/assignment4/myName.java
+++ Week-3/Assignments/src/main/java/com/yun_weidai/demo/assignment4/myName.java
@@ -4,12 +4,16 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+
 @Controller
 public class myName {
     @GetMapping("/myName")
-    public String getMyName(@CookieValue(value = "name" ,required = false) String nameInCookie, Model model) {
+    public String getMyName(@CookieValue(value = "name" ,required = false) String nameInCookie, Model model) throws UnsupportedEncodingException {
         if (nameInCookie != null && !nameInCookie.isEmpty()) { // name in cookie
-            model.addAttribute("name", nameInCookie); // access when template renders
+            model.addAttribute("name", URLDecoder.decode(nameInCookie, StandardCharsets.UTF_8.toString())); // access when template renders
             return "displayName"; // displayName.html
         } else {
             return "register"; // register.html
--- Week-3/Assignments/src/main/java/com/yun_weidai/demo/assignment4/setNameCookieController.java
+++ Week-3/Assignments/src/main/java/com/yun_weidai/demo/assignment4/setNameCookieController.java
@@ -5,14 +5,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+
 @Controller
 public class setNameCookieController {
     @GetMapping("/trackName")
-    public String nameUserSubmit(@RequestParam(value = "name", required = true) String name, HttpServletResponse response) {
+    public String nameUserSubmit(@RequestParam(value = "name", required = true) String name, HttpServletResponse response) throws UnsupportedEncodingException {
         if (name == null) { // failure case: null parameter
             return "redirect:/myName";   // get back to myName page
         }
-        Cookie nameCookie = new Cookie("name", name);  // create a new cookie object
+
+        Cookie nameCookie = new Cookie("name", URLEncoder.encode(name, StandardCharsets.UTF_8.toString()));  // create a new cookie object
         nameCookie.setMaxAge(24 * 60 * 60); // Cookie expires in 1 day
         nameCookie.setPath("/"); // set cookie path to root
         response.addCookie(nameCookie);  // Add cookie
