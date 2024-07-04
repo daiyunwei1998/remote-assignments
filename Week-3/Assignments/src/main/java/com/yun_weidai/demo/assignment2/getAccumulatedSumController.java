c016be4648e7c1712bdba7aada83a897746dd24b
Yunwei Dai <daiyunwei1998@gmail.com>
2024-07-04 15:22:00 +0800

    refactor: remove the unused CorrOrigin annotation

--- Week-3/Assignments/src/main/java/com/yun_weidai/demo/assignment2/getAccumulatedSumController.java
+++ Week-3/Assignments/src/main/java/com/yun_weidai/demo/assignment2/getAccumulatedSumController.java
@@ -1,6 +1,5 @@
 package com.yun_weidai.demo.assignment2;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,7 +7,6 @@ import java.math.BigInteger;
 
 @RestController
 public class getAccumulatedSumController {
-    @CrossOrigin
     @GetMapping("/data")
     public ResponseEntity<String> getAccumulatedSum(@RequestParam(value = "number", required = false) String numberString) {
         // if no parameter is specified "host:port/data"
