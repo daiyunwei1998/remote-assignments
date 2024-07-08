4bf968f9412d584d3f6804c6cf3f65d3226afbcb
Yunwei Dai <daiyunwei1998@gmail.com>
2024-07-08 09:37:18 +0800

    feat: assignment-1 complete

---
+++ Week-4/Assignment-1/asyncSchool.js
@@ -0,0 +1,37 @@
+/*
+A. Complete the function below to show a delayed result in the console
+*/
+function delayedResult(n1, n2, delayTime, callback) {
+  setTimeout(() => {
+    callback(n1 + n2);
+  }, delayTime);
+}
+delayedResult(4, 5, 3000, function (result) {
+  console.log(result);
+}); // 9 (4+5) will be shown in the console after 3 seconds
+delayedResult(-5, 10, 2000, function (result) {
+  console.log(result);
+}); // 5 (-5+10) will be shown in the console after 2 seconds
+
+/*
+B. To implement delayedResult again using promise this time. It should look like:
+*/
+function delayedResultPromise(n1, n2, delayTime) {
+  // your code here
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(n1 + n2);
+    }, delayTime);
+  });
+}
+delayedResultPromise(4, 5, 3000).then(console.log);
+// 9 (4+5) will be shown in the console after 3 seconds
+
+/* 
+C. To implement it using async/await this time.
+*/
+async function main() {
+  const result = await delayedResultPromise(4, 5, 3000);
+  console.log(result);
+}
+main(); // result will be shown in the console after <delayTime> seconds
